4e0b97df0fe973dbace6d8d3b3f038fec4ea4083
Michael Nolivos <michaelnolivos@gmail.com>
2022-08-09 18:14:11 -0400

    chore: setup rainbowkit provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from 'wagmi/providers/public';
+
+import '@rainbow-me/rainbowkit/styles.css';
 import '../styles/globals.css'
 
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.goerli],
+  [
+    infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_KEY  }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Eth3rdEye',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
 }
 
 export default MyApp
