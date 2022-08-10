06760c24a502f6528c9e6338fcd6289f587b7ac7
Michael Nolivos <michaelnolivos@gmail.com>
2022-08-09 22:48:36 -0400

    feat: targets

---
+++ src/targets.ts
@@ -0,0 +1,25 @@
+const targets = [
+  {
+    value: "land",
+  },
+  {
+    value: "water",
+  },
+  {
+    value: "manmade",
+  },
+  {
+    value: "natural",
+  },
+  {
+    value: "biological-organic",
+  },
+  {
+    value: "space-air",
+  },
+  {
+    value: "motion-energy"
+  },
+];
+
+export default targets;
